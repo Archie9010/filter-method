bfde7eb262c69f6377da03534883b2f511df2e3a
Archie9010 <archie9010@protonmail.com>
2022-10-19 07:54:20 +0000

    add code examples

--- filter.js
+++ filter.js
@@ -20,7 +20,11 @@ const people = [
     age: 18,
   },
 ];
+const oldEnough = people.filter(person => person.age <= 21);
+console.log(oldEnough);
 
+const paul = people.filter(p => p.name === "Paul")[0];
+console.log(paul);
 
 // Complex Filtering
 const students = [
